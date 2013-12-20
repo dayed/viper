293d0003d3f49c55d2eb282b8b7de77cee86d3ad
ollieread <me@ollieread.com>
2013-12-20 23:28:18 +0000

    Main route now throws an exception to be caught at a later date

--- app/routes.php
+++ app/routes.php
@@ -1,24 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function() {
-	return Viper\Controller\BaseController::error(
-			Config::get('response.method.code'), 
-			Config::get('response.method.http'), 
-			'No method supplied'
-	);
+/**
+ * Nothing should ever be given here. Sod off!
+ */
+Route::any('/', function() {
+	throw new Viper\Exception('No method supplied', 'method');
 });
-
+/**
+ * A nice little group, holding all the use related methods.
+ * 
+ * If you can't work out what each route does, despite the fact that in most
+ * the name is mentioned no less than three times, you shouldn't be looking
+ * at this file.
+ */
 Route::group(array('prefix' => '/user'), function() {
 	
 	Route::post('/login', array(
@@ -40,5 +34,12 @@ Route::group(array('prefix' => '/user'), function() {
 		'as'	=> 'user.register',
 		'uses'	=> 'UserController@register'
 	));
+	
+});
+/**
+ * This is for the admin panel!!
+ */
+Route::group(array('prefix' => '/admin'), function() {
+	
 	
 });
