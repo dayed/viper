5eb6cccddc17215327e1e577c8c8f3f1de16c7e3
Ollie Read <me@ollieread.com>
2014-01-14 23:31:21 +0000

    Made is so that the login and logout helper functions don't throw exceptions, as that'll be left up the code that calls them.

--- app/controllers/User/BaseController.php
+++ app/controllers/User/BaseController.php
@@ -1,12 +1,21 @@
 <?php
 
-use Viper\Exception as Viper_Exception;
 use Viper\Model\User_Token;
 
 class User_BaseController extends BaseController {
-	
-	protected function _login() {
-		if($this->user) {
+    /**
+     * Helper function for the simplification of logging a user in. This is
+     * called once we've done the manual verification, this is just simply for
+     * creating and assigning a new user token.
+     *
+     * @return User_Token
+     */
+    protected function _login() {
+        /**
+         * We've already got the user for this request, now we just need to set
+         * a token and respond so they're logged in for future requests.
+         */
+        if($this->user) {
 			$token = new User_Token;
 			$token->generate();
 			
@@ -14,9 +23,23 @@ class User_BaseController extends BaseController {
 				return $token;
 			}
 		}
+        /**
+         * We return null because this method should return an instance of User_Token,
+         * and false would be boolean. Blame this on the fact I've spent a week
+         * writing Java, and as you may know, Java is a bit of dick in regards
+         * to return types.
+         */
+        return null;
 	}
-	
-	protected function _logout() {
+
+    /**
+     * Helper function for the simplification of logging a user out. Also
+     * serves as a dual purpose, with a single way to remove a user token.
+     *
+     * @return bool
+     * @throws Viper\Exception
+     */
+    protected function _logout() {
 		if($this->user) {
 			/**
 			 * Grab the active token. We do this just incase there is another
@@ -33,19 +56,16 @@ class User_BaseController extends BaseController {
 				return true;
 			}
 			/**
-			 * If the user doesn't have an active token, we'll want to let
-			 * the developer know this, as this functionality shouldn't
-			 * be accessible without a token.
+			 * Since this is a helper function, we want to avoid throwing exceptions,
+             * and leave that up to the core code calling this.
 			 */
-			throw new Viper_Exception('No active token', 'token');
+			return false;
 		}
 		/**
-		 * If no token is provided, or the user property is null, we want
-		 * to throw an exception of token type, as this should not happen,
-		 * if it does, the developer is being naughty and calling this
-		 * incorrectly.
+		 * Like the above, we simply return a boolean response rather than throw an
+         * exception.
 		 */
-		throw new Viper_Exception('No token provided', 'token');
+		return false;
 	}
 	
 }
